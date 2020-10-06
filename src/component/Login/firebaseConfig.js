569f55c586effffabefb4730ebc3408f6faf97f5
bormillnayon <nayonbd15@gmail.com.com>
2020-10-06 18:31:38 +0600

    done my project

---
+++ src/component/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuFODZSbPuA3j5h4qBSHnp7KwsREAmr14",
+    authDomain: "volunteer-network-a77e6.firebaseapp.com",
+    databaseURL: "https://volunteer-network-a77e6.firebaseio.com",
+    projectId: "volunteer-network-a77e6",
+    storageBucket: "volunteer-network-a77e6.appspot.com",
+    messagingSenderId: "979408983818",
+    appId: "1:979408983818:web:87815b970d085e0d064cf3"
+  };
+  export default firebaseConfig;
